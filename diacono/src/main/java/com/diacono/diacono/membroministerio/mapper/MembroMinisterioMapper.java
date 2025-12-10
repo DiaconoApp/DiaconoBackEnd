8e32aa2501db4895a7a0e6997a902f984d2789b0
ricardoOliveiraN <ricardo.nicolau@sptech.school>
2025-12-10 19:08:10 -0300

    feat: adiciona idExterno na busca de membros - ministerio

--- diacono/src/main/java/com/diacono/diacono/membroministerio/mapper/MembroMinisterioMapper.java
+++ diacono/src/main/java/com/diacono/diacono/membroministerio/mapper/MembroMinisterioMapper.java
@@ -8,7 +8,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface MembroMinisterioMapper {
 
-
+    @Mapping(target = "idExterno", source = "membro.idExterno")
     @Mapping(target = "nome", source = "membro.nome")
     @Mapping(target = "email", source = "membro.email")
     @Mapping(target = "telefone", source = "membro.celular")
--- diacono/src/main/java/com/diacono/diacono/membroministerio/model/dto/response/MembroMinisterioDTO.java
+++ diacono/src/main/java/com/diacono/diacono/membroministerio/model/dto/response/MembroMinisterioDTO.java
@@ -4,8 +4,9 @@ import com.diacono.diacono.membro.model.entity.EnumStatusMembro;
 import com.diacono.diacono.membroministerio.model.entity.EnumCargoMembroMinisterio;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public record MembroMinisterioDTO(
-        String nome, String email, String telefone, EnumStatusMembro status, LocalDate dataNascimento, EnumCargoMembroMinisterio cargo, String nomeMinisterio
+        UUID idExterno, String nome, String email, String telefone, EnumStatusMembro status, LocalDate dataNascimento, EnumCargoMembroMinisterio cargo, String nomeMinisterio
 ) {
 }
