205461460c56586a49ebb503280cb170b7298d08
ricardoOliveiraN <ricardo.nicolau@sptech.school>
2025-12-23 14:57:04 -0300

    feat:inicio escala

--- diacono/src/main/java/com/diacono/diacono/escala/model/entity/Escala.java
+++ diacono/src/main/java/com/diacono/diacono/escala/model/entity/Escala.java
@@ -1,23 +1,31 @@
-//package com.diacono.diacono.escala.model.entity;
-//
-//import com.diacono.diacono.eventoministerio.model.entity.EventoMinisterio;
-//import com.diacono.diacono.global.util.IdEntityUtils;
-//import com.diacono.diacono.membroministerio.model.entity.MembroMinisterio;
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Entity
-//@Table(name = "escala", uniqueConstraints = @UniqueConstraint(columnNames = {"fk_evento_ministerio", "fk_membro_ministerio"}))
-//@Setter
-//@Getter
-//public class Escala extends IdEntityUtils {
-//
-//    @ManyToOne
-//    @JoinColumn(name = "fk_evento_ministerio", nullable = false)
-//    private EventoMinisterio eventoMinisterio;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "fk_membro_ministerio", nullable = false)
-//    private MembroMinisterio membroMinisterio;
-//}
+package com.diacono.diacono.escala.model.entity;
+
+import com.diacono.diacono.global.util.IdEntityUtils;
+import com.diacono.diacono.membro.model.entity.Membro;
+import com.diacono.diacono.ministerio.model.entity.Ministerio;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@SuperBuilder(toBuilder = true)
+public class Escala extends IdEntityUtils {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_ministerio", nullable = false)
+    private Ministerio ministerio;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "evento_ministerio",
+            joinColumns = @JoinColumn(name = "fk_escala"),
+            inverseJoinColumns = @JoinColumn(name = "fk_ministerio")
+    )
+    private Set<Membro> membros;
+
+}
