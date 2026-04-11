4d3c4235e73707cef19090aa58de93fa8116f30c
Fabiam <fabiam.silva@sptech.school>
2026-04-10 22:34:20 -0300

    feature: Alterado busca de variaveis do google para seguir clean arch

--- diacono/src/main/java/com/diacono/diacono/usecases/googleauth/LoginGoogleAuthService.java
+++ diacono/src/main/java/com/diacono/diacono/usecases/googleauth/LoginGoogleAuthService.java
@@ -2,11 +2,11 @@ package com.diacono.diacono.usecases.googleauth;
 
 import com.diacono.diacono.applications.dtos.googleauth.GoogleAuthRequestDTO;
 import com.diacono.diacono.applications.dtos.login.LoginResponseDTO;
+import com.diacono.diacono.global.config.GoogleOAuthProperties;
 import com.diacono.diacono.domain.entity.Membro;
 import com.diacono.diacono.global.error.exceptions.BadCredentialsException;
 import com.diacono.diacono.usecases.GenerateTokenUseCase;
 import com.diacono.diacono.usecases.membro.BuscarPorEmaiUseCase;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
@@ -21,14 +21,16 @@ public class LoginGoogleAuthService {
     private final JwtDecoder googleJwtDecoder;
     private final GenerateTokenUseCase generateTokenUseCase;
     private final BuscarPorEmaiUseCase buscarPorEmaiUseCase;
+    private final GoogleOAuthProperties googleOAuthProperties;
 
-    @Value("${google.oauth.client-id:}")
-    private String googleClientId;
-
-    public LoginGoogleAuthService(GenerateTokenUseCase generateTokenUseCase, BuscarPorEmaiUseCase buscarPorEmaiUseCase) {
+    public LoginGoogleAuthService(GenerateTokenUseCase generateTokenUseCase,
+                                  BuscarPorEmaiUseCase buscarPorEmaiUseCase,
+                                  GoogleOAuthProperties googleOAuthProperties
+    ) {
         this.googleJwtDecoder = JwtDecoders.fromOidcIssuerLocation("https://accounts.google.com");
         this.generateTokenUseCase = generateTokenUseCase;
         this.buscarPorEmaiUseCase = buscarPorEmaiUseCase;
+        this.googleOAuthProperties = googleOAuthProperties;
     }
 
     public LoginResponseDTO autenticar(GoogleAuthRequestDTO googleAuthRequestDTO) {
@@ -59,10 +61,14 @@ public class LoginGoogleAuthService {
 
     private void validarAudience(Jwt googleJwt) {
         List<String> audience = googleJwt.getAudience();
+        String googleClientId = googleOAuthProperties.clientId();
 
         if (googleClientId == null || googleClientId.isBlank()) {
             throw new BadCredentialsException("Configuracao do Google OAuth ausente na aplicacao");
         }
+
+        System.out.println("Audience do token do Google: " + audience);
+        System.out.println("Client ID esperado: " + googleClientId);
 
         if (audience == null || !audience.contains(googleClientId)) {
             throw new BadCredentialsException("Token do Google nao pertence a aplicacao");
